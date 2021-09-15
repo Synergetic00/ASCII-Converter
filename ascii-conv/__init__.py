517f960f523f69a5dc98afd8ef828ced2b3d6ebc
Synergetic00 <synergetic00@gmail.com>
2021-09-15 13:08:44 +1000

    v0.0.1

---
+++ ascii-conv/__init__.py
@@ -0,0 +1,16 @@
+bSerif = range(119808, 119833)
+iSerif = range(119860, 119885)
+biSerif = range(119912, 119937)
+sansSerif = range(120224, 120249)
+bSansSerif = range(120276, 120301)
+iSansSerif = range(120328, 120353)
+biSansSerif = range(120380, 120405)
+script = range(119964, 119989)
+bScript = range(120016, 120041)
+fraktur = range(120068, 120093)
+bFraktur = range(120172, 120197)
+monospace = range(120432, 120457)
+bMonospace = range(120120, 120145)
+
+def asciify(input):
+    return input
